03e6b5bb1ba82be20f03dbdcfb613a6e9411d1e1
zulfiker20 <zbadsha1991@gmail.com>
2024-12-08 12:14:17 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    "use strict";
+
+    // offcanvas bar 
+    $(".toogle-btn").on('click', function() {
+      $('.show-nav').toggleClass('block hidden');
+
+  });
+
+})(jQuery);
